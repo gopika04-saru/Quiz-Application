d99fd765fb9c85fd878075de64a4adfeb029f132
Gopika <kotakalagopika@gmail.com>
2025-04-01 16:53:50 +0530

    Initial commit

---
+++ src/main/java/com/example/QuizApp/model/Quiz.java
@@ -0,0 +1,47 @@
+package com.example.QuizApp.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    private String title;
+
+    @ManyToMany
+    private List<Question> questions;
+
+    public Integer getId() {
+        return Id;
+    }
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    @Override
+    public String toString() {
+        return "Quiz{" +
+                "Id=" + Id +
+                ", title='" + title + '\'' +
+                ", questions=" + questions +
+                '}';
+    }
+}
